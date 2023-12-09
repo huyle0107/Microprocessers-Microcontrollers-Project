0f95e5d6a1076ea4aeff2b0b94d3884e8ea2b38d
dangpham1310 <148855149+dangpham1310@users.noreply.github.com>
2023-12-09 22:19:15 +0700

    add mode_manual and mode_tuning

---
+++ Core/Inc/mode_manual.h
@@ -0,0 +1,13 @@
+/*
+ * mode_2.h
+ *
+ *  Created on: Nov 12, 2023
+ *      Author: Admin
+ */
+
+#ifndef INC_MODE_MANUAL_H_
+#define INC_MODE_MANUAL_H_
+
+void manual_mode();
+
+#endif /* INC_MODE_MANUAL_H_ */
---
+++ Core/Inc/mode_tuning.h
@@ -0,0 +1,13 @@
+/*
+ * add_time_button.h
+ *
+ *  Created on: Nov 12, 2023
+ *      Author: Admin
+ */
+
+#ifndef INC_MODE_TUNING_H_
+#define INC_MODE_TUNING_H_
+
+void check_button_add_time();
+
+#endif /* INC_MODE_TUNING_H_ */
